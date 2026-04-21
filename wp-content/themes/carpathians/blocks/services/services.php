68429f09255f9418706f6122d04360e2278e69c1
BakaloMykhailo <11925228+BakaloMykhailo@users.noreply.github.com>
2026-04-21 04:22:44 +0300

    Service refactor

--- wp-content/themes/carpathians/blocks/services/services.php
+++ wp-content/themes/carpathians/blocks/services/services.php
@@ -9,34 +9,65 @@
     <?php endif; ?>
 
     <?php if ( $services ) : ?>
-        <div class="flex flex-nowrap gap-[1px] md:hidden" data-tabs>
-            <?php foreach ( $services as $index => $service ) : ?>
-                <?php
-                    $title       = get_the_title( $service );
-                    $color       = get_field( 'color', $service->ID );
-                    $thumbnail   = get_the_post_thumbnail_url( $service->ID, 'full' );
-                    $hover_image = get_field( 'hover_image', $service->ID );
-                ?>
-                <div
-                    class="group p-3.5 basis-0 grow flex items-center justify-center cursor-pointer transition-colors duration-300"
-                    style="--hover-color: <?php echo esc_attr( $color ?: '#000' ); ?>;"
-                    role="tab"
-                    data-tab="<?php echo $index; ?>"
-                >
-                    <div class="max-h-12 max-w-12 relative">
-                        <?php if ( $thumbnail ) : ?>
-                            <img src="<?php echo esc_url( $thumbnail ); ?>" alt="<?php echo esc_attr( $title ); ?>" class="object-cover w-full h-full transition-opacity duration-300 group-[.is-active]:opacity-0">
-                        <?php endif; ?>
-                        <?php if ( $hover_image ) : ?>
-                            <img src="<?php echo esc_url( $hover_image['url'] ); ?>" alt="<?php echo esc_attr( $title ); ?>" class="object-cover w-full h-full absolute inset-0 opacity-0 transition-opacity duration-300 group-[.is-active]:opacity-100">
-                        <?php endif; ?>
+
+        <?php /* Mobile */ ?>
+        <div class="md:hidden">
+            <div class="flex flex-nowrap" data-tabs>
+                <?php foreach ( $services as $index => $service ) : ?>
+                    <?php
+                        $title       = get_the_title( $service );
+                        $color       = get_field( 'color', $service->ID );
+                        $thumbnail   = get_the_post_thumbnail_url( $service->ID, 'full' );
+                        $hover_image = get_field( 'hover_image', $service->ID );
+                    ?>
+                    <div
+                        class="p-3.5 basis-0 grow flex items-center justify-center cursor-pointer transition-colors duration-300"
+                        style="--hover-color: <?php echo esc_attr( $color ?: '#000' ); ?>; <?php echo $index === 0 ? 'background-color: ' . esc_attr( $color ?: '#000' ) . ';' : ''; ?>"
+                        role="tab"
+                        data-tab="<?php echo $index; ?>"
+                    >
+                        <div class="max-h-12 max-w-12 relative">
+                            <?php if ( $thumbnail ) : ?>
+                                <img src="<?php echo esc_url( $thumbnail ); ?>" alt="<?php echo esc_attr( $title ); ?>" class="object-cover w-full h-full transition-opacity duration-300<?php echo $index === 0 ? ' opacity-0' : ''; ?>" data-default-img>
+                            <?php endif; ?>
+                            <?php if ( $hover_image ) : ?>
+                                <img src="<?php echo esc_url( $hover_image['url'] ); ?>" alt="<?php echo esc_attr( $title ); ?>" class="object-cover w-full h-full absolute inset-0 transition-opacity duration-300<?php echo $index === 0 ? '' : ' opacity-0'; ?>" data-hover-img>
+                            <?php endif; ?>
+                        </div>
                     </div>
-                </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            </div>
+
+            <div data-tab-panels>
+                <?php foreach ( $services as $index => $service ) : ?>
+                    <?php
+                        $title       = get_the_title( $service );
+                        $description = get_field( 'description', $service->ID );
+                        $color       = get_field( 'color', $service->ID );
+                        $thumbnail   = get_the_post_thumbnail_url( $service->ID, 'full' );
+                        $hover_image = get_field( 'hover_image', $service->ID );
+                    ?>
+                    <div
+                        class="p-10 min-h-97.5 <?php echo $index === 0 ? 'flex' : 'hidden'; ?> items-center justify-center"
+                        style="--hover-color: <?php echo esc_attr( $color ?: '#000' ); ?>; <?php echo $index === 0 ? 'background-color: ' . esc_attr( $color ?: '#000' ) . ';' : ''; ?>"
+                        data-tab-panel="<?php echo $index; ?>"
+                    >
+                        <div class="relative max-h-40 max-w-40">
+                            <?php if ( $thumbnail ) : ?>
+                                <img src="<?php echo esc_url( $thumbnail ); ?>" alt="<?php echo esc_attr( $title ); ?>" class="object-cover w-full h-full transition-opacity duration-300<?php echo $index === 0 ? ' opacity-0' : ''; ?>" data-default-img>
+                            <?php endif; ?>
+                            <?php if ( $hover_image ) : ?>
+                                <img src="<?php echo esc_url( $hover_image['url'] ); ?>" alt="<?php echo esc_attr( $title ); ?>" class="object-cover w-full h-full absolute inset-0 transition-opacity duration-300<?php echo $index === 0 ? '' : ' opacity-0'; ?>" data-hover-img>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
 
-        <div class="flex flex-nowrap gap-[1px]" data-tab-panels>
-            <?php foreach ( $services as $index => $service ) : ?>
+        <?php /* Desktop */ ?>
+        <div class="hidden md:flex flex-nowrap border-gray border-t border-b">
+            <?php foreach ( $services as $service ) : ?>
                 <?php
                     $title       = get_the_title( $service );
                     $description = get_field( 'description', $service->ID );
@@ -45,16 +76,15 @@
                     $hover_image = get_field( 'hover_image', $service->ID );
                 ?>
                 <div
-                    class="group p-10 basis-0 grow min-h-97.5 md:min-h-151 lg:min-h-188.5 flex items-center justify-center transition-all duration-300 hover:hover-color md:hover:basis-1/5 cursor-pointer <?php echo $index === 0 ? '' : 'hidden md:flex'; ?>"
+                    class="group border-r border-r-gray last:border-r-0 p-10 basis-0 grow min-h-151 lg:min-h-188.5 flex items-center justify-center transition-all duration-300 hover:hover-color hover:basis-1/5 cursor-pointer"
                     style="--hover-color: <?php echo esc_attr( $color ?: '#000' ); ?>;"
-                    data-tab-panel="<?php echo $index; ?>"
                 >
-                    <div class="relative max-h-40 max-w-40">
+                    <div class="relative max-h-40 max-w-40 aspect-square">
                         <?php if ( $thumbnail ) : ?>
-                            <img src="<?php echo esc_url( $thumbnail ); ?>" alt="<?php echo esc_attr( $title ); ?>" class="object-cover w-full h-full transition-opacity duration-300 group-hover:opacity-0 group-[.is-active]:opacity-0">
+                            <img src="<?php echo esc_url( $thumbnail ); ?>" alt="<?php echo esc_attr( $title ); ?>" class="object-cover w-full h-full transition-opacity duration-300 group-hover:opacity-0">
                         <?php endif; ?>
                         <?php if ( $hover_image ) : ?>
-                            <img src="<?php echo esc_url( $hover_image['url'] ); ?>" alt="<?php echo esc_attr( $title ); ?>" class="object-cover w-full h-full absolute inset-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-[.is-active]:opacity-100">
+                            <img src="<?php echo esc_url( $hover_image['url'] ); ?>" alt="<?php echo esc_attr( $title ); ?>" class="object-cover w-full h-full absolute inset-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
                         <?php endif; ?>
                     </div>
                     <div class="opacity-0 absolute text-center">
@@ -63,5 +93,6 @@
                 </div>
             <?php endforeach; ?>
         </div>
+
     <?php endif; ?>
 </section>
